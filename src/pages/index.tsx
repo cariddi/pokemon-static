b940dbb6bb8b376dacd0174407a724927c8cd467
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2023-03-16 19:24:14 -0300

    adding next ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@nextui-org/react";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
 	return (
 		<>
 			<h1>Hello World</h1>
+
+			<Button color="gradient">Click Me!</Button>
 		</>
 	);
 };
