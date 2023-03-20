fa45bf16fdcac1e755b1b8080ca7a46af0749612
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2023-03-20 11:25:12 -0300

    adding getStaticProps + get static pokemons request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
 import { NextPage, GetStaticProps } from "next";
+import { PokemonList, Pokemon } from "../../interfaces";
+import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
 
-const HomePage: NextPage = (props) => {
-	console.log({ props });
+interface HomeProps {
+	pokemons: Pokemon[];
+}
+
+const HomePage: NextPage<HomeProps> = ({ pokemons }) => {
+	if (!pokemons) return <>No Pokemons for now :s</>;
+
 	return (
 		<Layout title="Pokemon List">
-			<ul>
-				<li>Pokemon</li>
-				<li>Pokemon</li>
-				<li>Pokemon</li>
-				<li>Pokemon</li>
-				<li>Pokemon</li>
-			</ul>
+			{
+				<ul>
+					{pokemons.map((pokemon) => (
+						<li key={pokemon.url}>{pokemon.name}</li>
+					))}
+				</ul>
+			}
 		</Layout>
 	);
 };
@@ -22,10 +29,13 @@ const HomePage: NextPage = (props) => {
 //- The data can be publicly cached (not user-specific).
 //- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
 export const getStaticProps: GetStaticProps = async (ctx) => {
-	console.log("hello world");
+	const {
+		data: { results },
+	} = await pokeApi.get<PokemonList>("/pokemon?limit=151");
+
 	return {
 		props: {
-			name: "Lucas",
+			pokemons: results,
 		},
 	};
 };
